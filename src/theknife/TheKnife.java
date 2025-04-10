d044a654a979ca829ef5e7954a987cfcc36341c0
AleFrigio <alefrigio02@gmail.com>
2025-04-10 17:13:37 +0200

    merda

--- src/theknife/TheKnife.java
+++ src/theknife/TheKnife.java
@@ -1,33 +1,21 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 package theknife;
 
 /**
  *
- * @author davim
+ * @author Alefr
  */
 public class TheKnife {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
     }
-    
-} culotette
-        
+}
 
 
-skibiidiskibidiskibidihawktuah
-
-
-ginantesche palle
-
-
-ODIOINE
-
-
-skifoso neg
